80def45e1b57e1e8ac12f0e0b4b6efcc564f7fdb
Albert <albert.lam.dev@gmail.com>
2025-04-22 00:03:40 -0400

    init

--- main.tf
+++ main.tf
@@ -37,14 +37,14 @@ resource "aws_elasticache_subnet_group" "this" {
 
 resource "aws_elasticache_cluster" "this" {
   cluster_id           = random_id.cluster_id.hex
-  engine              = "redis"
-  node_type           = var.node_type
-  num_cache_nodes     = 1
-  parameter_group_name = "default.redis7.1"
-  port                = 6379
-  subnet_group_name   = aws_elasticache_subnet_group.this.name
-  security_group_ids  = [aws_security_group.redis.id]
-  engine_version      = "7.1"
+  engine               = "redis"
+  node_type            = var.node_type
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.this.name
+  security_group_ids   = [aws_security_group.redis.id]
+  engine_version       = "7.1"
 
   tags = merge(
     {
